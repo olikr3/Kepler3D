3f77637b4d53201e85f07a76da378ab5a6fdb747
olikr3 <oliver.kremser@gmail.com>
2024-09-16 13:06:42 +0200

    updates

--- src/game_world.rs
+++ src/game_world.rs
@@ -2,11 +2,30 @@ mod rigid_body;
 
 pub struct GameWorld {
     objects: Vec<RigidBody>,
+    gravity: f64,
 }
 
 impl GameWorld {
-    
+
     fn new() -> Self {
-        Self { {Vec::new()} }
+        Self {
+            objects: Vec::new(),
+            gravity: 9.81,
+        }
+    }
+
+    fn add_body(&mut self, body: RigidBody) {
+        self.objects.push(body);
+    }
+
+    // simulate the world for 'dt' seconds
+    fn simulate(&mut self, dt: usize) {
+        for _ in 0..dt {
+            todo!()
+        }
+    }
+
+    fn disable_gravity(&mut self) {
+        self.gravity = 0.0;
     }
 }
--- src/rigid_body.rs
+++ src/rigid_body.rs
@@ -5,4 +5,22 @@ pub struct RigidBody {
     radius: f32,
     pos: Vector3,
     velocity: Vector3,
+    //force: Vector3,
+    //torque: Vector3,
+    //shape: Shape -- for the future...
+}
+
+impl RigidBody {
+
+    fn new(mass: f32, radius:f32, pos: Vector3, velocity: Vector3) -> Self {
+        Self { mass, radius, pos, velocity }
+    }
+
+    fn compute_force_and_torque(&self) {
+        todo!()
+    }
+
+    fn accelerate(acc: Vector3) {
+        todo!()
+    }
 }
